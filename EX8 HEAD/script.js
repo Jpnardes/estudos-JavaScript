16601c48ec1a85514d03659daab7bb2ae50f7333
João Pedro Nardes <nardes423@gmail.com>
2023-05-18 21:30:24 -0300

    finalizando primeiros exemplos

--- EX8 HEAD/script.js
+++ EX8 HEAD/script.js
@@ -1,13 +1,19 @@
 //este código vai ser executado antes dos elementos do body existirem
 
 
-document.addEventListener('readystatechange', (event) => {
-    if (event.target.readyState === 'complete') {
+//document.addEventListener('readystatechange', (event) => {
+   // if (event.target.readyState === 'interactive') {
 
-        document.querySelector("button").addEventListener('click', () => {
+       // document.querySelector("button").addEventListener('click', () => {
 
-            document.querySelector('h1').innerText = "Texto do título alterado"
+      //      document.querySelector('h1').innerText = "Texto do título alterado"
             
-        })
-    }
+  //      })
+ //   }
+//})
+
+document.addEventListener('DOMContentLoaded', () =>{
+    document.querySelector("button").addEventListener('click', () => {
+        document.querySelector('h1').innerText = "Texto do título alterado"
+    })
 })
