d505829cb4fbd3b154e1a6786588fa97d9141913
João Pedro Nardes <nardes423@gmail.com>
2023-07-06 20:47:52 -0300

    estudo de arrays

---
+++ exercicios/Arrays/app.js
@@ -0,0 +1,111 @@
+//let nomes = ["joao","ana","carlos"];
+
+//Adiciona elementos no fim do array
+//nomes.push("joaquim")
+//console.table(nomes)
+
+//Adiciona elementos no inicio do array
+
+//nomes.unshift("Pedro")
+//console.table(nomes)
+
+//Remove elementos no começo do array
+
+//nomes.shift()
+//console.table(nomes)
+
+//Remover elementos do fim do array
+
+//nomes.pop()
+//console.table(nomes)
+
+// Remover elementos permite guardar o valor removido em uma variável
+
+//let removido = nomes.pop()
+//console.log(removido)
+//console.table(nomes)
+
+//Remover e/ou adicionar elementos em qualquer posição do array
+
+//console.log(nomes)
+//nomes.splice(1,2, "joaquim", "manuel","Rui");
+//console.table(nomes)
+
+//ver eliminados
+//console.table(nomes)
+//let eliminados = nomes.splice(1, 2, "joaquim", "manuel","Rui");
+//console.table(nomes)
+//console.table(eliminados)
+
+//Extrair partes de um array( Retorna um nome array)
+//console.table(nomes)
+//let nomes_extraidos = nomes.slice(1,2)
+//console.table(nomes_extraidos)
+
+// Juntar dois arrays
+
+//console.table(nomes)
+//let outros_nomes = ["joaquim", "carla", "manuel"]
+//nome = nomes.concat(outros_nomes)
+//console.table(nomes)
+
+// procurar item num array
+
+//console.log(nomes.includes("carlos"))
+
+// Procurar posição de item num array
+
+//console.table(nomes)
+//console.log(nomes.indexOf("joao"))
+
+//Converter um array numa string
+
+//let todos_os_nomes = nomes.join("-")
+//console.log(todos_os_nomes)
+
+//**Important= Outros métodos envolvem Função e metodologias! */
+
+//--------------------------------------------------------
+
+//COMO ORDENAR ARRAYS
+
+//let nomes = ["João", "Ana", "Carlos", "Xavier", "Catarina", "Fernanda"]
+
+//ordenar um array de forma ascendente * Altera a estrutura da array
+
+//nomes.sort()
+//console.table(nomes)
+
+//Ordenar um array de forma descendente
+
+//nomes.sort()
+//nomes.reverse()
+//console.table(nomes)
+
+//Sintaxe alternativa 
+
+//nomes.sort().reverse()
+//console.table(nomes)
+
+//(2) Sintaxe Alternativa
+
+//console.table(nomes.sort().reverse())
+
+//Também Funciona para arrays numéricas, mas..
+
+//let valores = [1,30,20,50,56,45]
+//valores.sort()
+//console.table(valores)
+//valores.reverse()
+//console.table(valores)
+
+//let valores = [1,30,20,100,50,56,45]
+//valores.sort()
+//console.table(valores)
+//valores.reverse()
+//console.table(valores)
+
+
+//Existem formas de fazer a Ordenação correta, mas envolvem Funções
+
+
