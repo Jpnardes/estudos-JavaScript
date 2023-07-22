d76ba350b20300644c672a38e4decb119826d1a4
João Pedro Nardes <nardes423@gmail.com>
2023-07-22 18:03:09 -0300

    exercicios

---
+++ estudos/event listener/remover-event/script.js
@@ -0,0 +1,17 @@
+/* 
+OBJETIVO:
+Ao clicar no botão, alterar a cor de fundo da caixa para aquamarine,
+apresentar o texto 'clique' na consola e
+remover o evento click do botão
+*/
+let botao = document.querySelector('button')
+let caixa = document.querySelector('.caixa');
+
+function executar(event){
+    caixa.classList.add('cor-fundo-caixa');
+    console.log('clique')
+    botao.removeEventListener('click', executar)
+
+}
+
+botao.addEventListener('click',executar)
