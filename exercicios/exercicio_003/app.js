134367fb63366cc3fd38e6a6fb8217e3d7274268
João Pedro Nardes <nardes423@gmail.com>
2023-07-27 21:24:42 -0300

    modified:   exercicios/exercicio_003/app.js 	modified:   exercicios/exercicio_004/app.js

--- exercicios/exercicio_003/app.js
+++ exercicios/exercicio_003/app.js
@@ -10,3 +10,27 @@ Enunciado: CONTADOR COM CORES PARA NEGATIVOS, ZERO E POSITIVOS
     No caso do valor ser positivo o texto deve ser verde puro.
 
 ---------------------------------------------------------------------------- */
+let valor = 0;
+const h1_valor = document.querySelector('#valor');
+
+document.querySelector('#btn_incremento').addEventListener('click', ()=>{
+    set_valor()
+    ++valor;
+})
+document.querySelector('#btn_decremento').addEventListener('click', ()=>{
+    set_valor()
+    --valor;
+})
+
+
+function set_valor(){
+    h1_valor.textContent = valor;
+    if(valor===0){
+        h1_valor.style.color = 'white';
+    }
+    else if(valor > 0){
+        h1_valor.style.color = 'green';
+    }else if(valor < 0){
+        h1_valor.style.color = 'red';
+    }
+}
--- exercicios/exercicio_004/app.js
+++ exercicios/exercicio_004/app.js
@@ -9,3 +9,26 @@ Enunciado: TRÊS SLIDERS COM VALORES INDEPENDENTES
     definidas através do JavaScript.
 
 ---------------------------------------------------------------------------- */
+//value_1
+//range_1
+const range1 = document.querySelector('#range_1');
+range1.setAttribute('min', 0);
+range1.setAttribute('max', 100);
+range1.value = 0
+range1.addEventListener('input', (event) =>{
+    document.querySelector('#value_1').textContent = event.target.value;
+})
+const range2 = document.querySelector('#range_2');
+range2.setAttribute('min', 0);
+range2.setAttribute('max', 100);
+range2.value = 0
+range2.addEventListener('input', (event) =>{
+    document.querySelector('#value_2').textContent = event.target.value;
+})
+const range3 = document.querySelector('#range_3');
+range3.setAttribute('min', 0);
+range3.setAttribute('max', 100);
+range3.value = 0
+range3.addEventListener('input', (event) =>{
+    document.querySelector('#value_3').textContent = event.target.value;
+})
