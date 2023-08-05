46995880f393e3f9ef3b0cda449a1e326e308214
João Pedro Nardes <nardes423@gmail.com>
2023-08-05 10:46:56 -0300

    modified:   exercicios/exercicio_008/app.js

--- exercicios/exercicio_008/app.js
+++ exercicios/exercicio_008/app.js
@@ -6,3 +6,20 @@ Enunciado:
     A ideia é criar o código javascript que permita que o clique em cada
     um dos botões altere o texto acima para a cor de fundo do botão.
 ---------------------------------------------------------------------------- */
+let texto = document.querySelector('h3');
+
+document.querySelector('#btn1').addEventListener('click',()=>{
+    texto.className = 'text-primary'
+})
+document.querySelector('#btn2').addEventListener('click',()=>{
+    texto.className = 'text-info'
+})
+document.querySelector('#btn3').addEventListener('click',()=>{
+    texto.className = 'text-success'
+})
+document.querySelector('#btn4').addEventListener('click',()=>{
+    texto.className = 'text-danger'
+})
+document.querySelector('#btn5').addEventListener('click',()=>{
+    texto.className = 'text-warning'
+})
