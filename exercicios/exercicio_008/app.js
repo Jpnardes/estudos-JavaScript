4beb3f9d6411cf10f428ebb2cab49f55f334f68a
João Pedro Nardes <nardes423@gmail.com>
2023-08-06 19:19:05 -0300

    modified:   exercicios/exercicio_008/app.js 	modified:   exercicios/exercicio_009/app.js 	modified:   exercicios/exercicio_010/app.js

--- exercicios/exercicio_008/app.js
+++ exercicios/exercicio_008/app.js
@@ -6,20 +6,28 @@ Enunciado:
     A ideia é criar o código javascript que permita que o clique em cada
     um dos botões altere o texto acima para a cor de fundo do botão.
 ---------------------------------------------------------------------------- */
-let texto = document.querySelector('h3');
+// let texto = document.querySelector('h3');
 
-document.querySelector('#btn1').addEventListener('click',()=>{
-    texto.className = 'text-primary'
-})
-document.querySelector('#btn2').addEventListener('click',()=>{
-    texto.className = 'text-info'
-})
-document.querySelector('#btn3').addEventListener('click',()=>{
-    texto.className = 'text-success'
-})
-document.querySelector('#btn4').addEventListener('click',()=>{
-    texto.className = 'text-danger'
-})
-document.querySelector('#btn5').addEventListener('click',()=>{
-    texto.className = 'text-warning'
+// document.querySelector('#btn1').addEventListener('click',()=>{
+//     texto.className = 'text-primary'
+// })
+// document.querySelector('#btn2').addEventListener('click',()=>{
+//     texto.className = 'text-info'
+// })
+// document.querySelector('#btn3').addEventListener('click',()=>{
+//     texto.className = 'text-success'
+// })
+// document.querySelector('#btn4').addEventListener('click',()=>{
+//     texto.className = 'text-danger'
+// })
+// document.querySelector('#btn5').addEventListener('click',()=>{
+//     texto.className = 'text-warning'
+// })
+
+const cores = ['primary', 'info', 'success', 'danger', 'warning'];
+document.querySelectorAll('[id^="btn"]').forEach((btn) => {
+    btn.addEventListener('click', (btn) => {
+        let index = btn.target.id.substring(3, 4) - 1;
+        document.querySelector('h3').className = 'text-'+ cores[index];
+    })
 })
