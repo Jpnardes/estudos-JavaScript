730f309190b10875c48fc5861b0de3ad0e23b3f3
João Pedro Nardes <nardes423@gmail.com>
2023-08-18 19:51:23 -0300

    commit

--- exercicios/exercicio_025/app.js
+++ exercicios/exercicio_025/app.js
@@ -7,3 +7,23 @@ Enunciado:
     no div cujo id = resultados.
 ---------------------------------------------------------------------------- */
 
+const resultados = document.querySelector('#resultados');
+const text_numero = document.querySelector('#text_numero');
+text_numero.setAttribute('min', 1)
+text_numero.setAttribute('max', 20)
+
+tabuada(1);
+
+text_numero.addEventListener('change', () => {
+    tabuada(text_numero.value);
+
+})
+
+function tabuada(valor) {
+    let html = "";
+    for (let i = 1; i <= 10; i++) {
+        html += ` ${valor} x ${i} = ${valor * i} <br>`;
+    }
+    resultados.innerHTML = html;
+
+}
